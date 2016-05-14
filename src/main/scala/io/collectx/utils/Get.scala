bbdb5983298ed942a6a880fbe0daa8f2a0fe9050
Anwar Abdus-Samad <jabdussa@gmail.com>
2016-05-14 03:11:30 -0400

    Renaming Files

---
+++ src/main/scala/io/collectx/utils/Get.scala
@@ -0,0 +1,39 @@
+package io.collectx.gimme
+
+import akka.actor.Actor
+import akka.actor.ActorLogging
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.stream.ActorMaterializerSettings
+import akka.util.ByteString
+
+
+class Get extends Actor
+  with ActorLogging {
+ 
+  import akka.pattern.pipe
+  import context.dispatcher
+  
+  final implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(context.system))
+ 
+  val http = Http(context.system)
+ 
+  override def preStart() = {
+    http.singleRequest(HttpRequest(uri = "http://akka.io"))
+      .pipeTo(self)
+  }
+ 
+  def receive = {
+    case HttpResponse(StatusCodes.OK, headers, entity, _) =>
+      log.info("Got response, body: " + entity.dataBytes.runFold(ByteString(""))(_ ++ _))
+      
+    case HttpResponse(code, _, _, _) =>
+      log.info("Request failed, response code: " + code)
+      
+    case _  =>
+      log.info("Nada...")
+      
+  }
+ 
+}
