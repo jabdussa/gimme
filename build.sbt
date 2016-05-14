df25ef9209ed7396dc49e006e143dfac20ad6578
Anwar Abdus-Samad <jabdussa@gmail.com>
2016-05-14 13:12:15 -0400

    updates

--- build.sbt
+++ build.sbt
@@ -4,10 +4,13 @@ version := "1.0"
  
 scalaVersion := "2.11.7"
 
+assemblyJarName in assembly := "something.jar"
+
 libraryDependencies ++= 
 Seq(	
-	 "com.typesafe.akka" %% "akka-actor" % "2.4.4"
-	,"com.typesafe.akka" %% "akka-http-core" % "2.4.4"
-	,"com.typesafe.akka" %% "akka-slf4j" % "2.4.4"
-	,"ch.qos.logback" % "logback-classic" % "1.1.7"
+	 "com.typesafe.akka" 	%% "akka-actor"		% "2.4.4"
+	,"com.typesafe.akka" 	%% "akka-http-core" 	% "2.4.4"
+	,"com.typesafe.akka" 	%% "akka-slf4j"		% "2.4.4"
+	,"ch.qos.logback"	% "logback-classic" 	% "1.1.7"
+	,"org.jsoup"		% "jsoup"		% "1.9.1"
 )
---
+++ src/main/scala/io/collectx/gimme/Load.scala
@@ -0,0 +1,52 @@
+package io.collectx.gimme
+
+import akka.actor.Actor
+import akka.actor.ActorLogging
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.stream.ActorMaterializerSettings
+import akka.util.ByteString
+import com.typesafe.config.ConfigFactory
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+class Get extends Actor
+  with ActorLogging {
+ 
+  import akka.pattern.pipe
+  import context.dispatcher
+  
+  final implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(context.system))
+ 
+  val conf = ConfigFactory.load()
+  val http = Http(context.system)
+  
+  override def preStart() = {
+    
+    log.info(conf.getString("gimme.hello"))
+    
+    log.info("Parse Settings")
+    log.info("Manufacturers: " + conf.getString("gimme.parse.manufacturers") )
+    
+    http.singleRequest(HttpRequest(uri = conf.getString("gimme.url") ))
+      .pipeTo(self)
+  }
+ 
+  def receive = {
+    case HttpResponse(StatusCodes.OK, headers, entity, _) =>
+      log.info("Got response, body: " + entity.dataBytes.runFold(ByteString(""))(_ ++ _))
+      
+    case HttpResponse(code, _, _, _) =>
+      log.info("Request failed, response code: " + code)
+      
+    case _  =>
+      log.info("I got's nada...")
+      
+  }
+ 
+}
